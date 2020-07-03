2a60e4a767cc15f542a2aa7cbabcf7433fb44398
clgerhardt <lieblegerhardt@gmail.com>
2020-07-02 23:27:11 -0400

    messing with main app page for type writer and katicism logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
-function App() {
+import KaticismLogoWhite from './assets/katicismLogos/Primary_White Logo.png';
+import './App.css';
+import Typewriter from 'typewriter-effect/dist/core';
+ 
+
+const App = () => {
+  useEffect(() => {
+
+    const typewriter = new Typewriter("#title", {
+      autoStart: true,
+      deleteChars: 0
+    })
+    typewriter
+      .typeString('Christian Gerhardt,')
+      .pauseFor(1000)
+      .typeString('<br>Software Developer,')
+      .pauseFor(1000)
+      .typeString('<br>Lover of Coffee')
+      .pauseFor(1000)
+      .typeString('<br>& Casual Streamer')
+      .start()
+
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p id="title">
         </p>
+        <img src={KaticismLogoWhite} className="App-logo" alt="logo" />
+        <p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://twitch.tv/katicism"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          https://twitch.tv/katicism
         </a>
+        </p>
       </header>
     </div>
   );
